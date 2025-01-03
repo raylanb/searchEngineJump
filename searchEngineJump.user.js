2b7a047e7b140b11d66bb38512a3a9f145ac19bd
raylanb <56618618+raylanb@users.noreply.github.com>
2025-01-03 20:17:19 +0800

    docs(README.md): update README.md

--- searchEngineJump.user.js
+++ searchEngineJump.user.js
@@ -3,9 +3,9 @@
 // @author         NLF&锐经(修改) & iqxin(修改) & raylanb(修改)
 // @contributor    iqxin
 // @description    方便的在各个搜索引擎之间跳转,增加可视化设置菜单,能更友好的自定义设置,修复百度搜索样式丢失的问题
-// @version        5.26.11
+// @version        5.26.12
 // @created        2011-07-02
-// @lastUpdated    2024-07-05
+// @lastUpdated    2025-01-03
 
 // @origin_namespace      https://greasyfork.org/zh-CN/scripts/27752-searchenginejump
 // @updateURL       https://raw.githubusercontent.com/raylanb/searchEngineJump/master/searchEngineJump.user.js
